2b88209111a491a1f9afc9c2a36e6a4a909d7b90
andrenobs <79150599+andrenobs@users.noreply.github.com>
2021-02-20 15:22:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
-let p = 0
-let D = 0
-let last_p = 0
 let dif = 0
+let last_p = 0
+let D = 0
+let p = 0
+AlphaBot2.Run(Dir.stop, 0)
+basic.pause(1000)
+AlphaBot2.SensorCalibrated()
 basic.forever(function () {
     p = AlphaBot2.readLine() - 2000
     D = p / last_p
     last_p = p
     dif = p / 20 + D / 5
+    dif = Math.max(dif, -120)
+    dif = Math.min(dif, 120)
+    if (dif > 0) {
+        AlphaBot2.MotorRun(Motors.M1, 120)
+        AlphaBot2.MotorRun(Motors.M2, 120 - dif)
+    } else {
+        AlphaBot2.MotorRun(Motors.M1, 120 / dif)
+        AlphaBot2.MotorRun(Motors.M2, 120)
+    }
 })
